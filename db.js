fc8bd5d541e249cd6e483bbfc0c326c2b7cc45ed
gavingiddings1997@gmail.com <Gavin>
2023-02-27 17:50:14 -0500

    db

--- db.js
+++ db.js
@@ -1,21 +1,46 @@
 const { Pool } = require('pg')
 
-const connectionLocal = {
-  user: 'postgres',
-  host: 'localhost',
-  database: 'assembly-backend',
-  password: '',
-  port: 5432,
-}
+const connectionString = 'postgresql://postgres:f1ynjwcZwvCVSbPqlFAH@containers-us-west-32.railway.app:7290/railway'
 
-// this line of code is for heroku or netify to run the backend from a third party host
-const connectionProduction = {
-  connectionString: process.env.DATABASE_URL, 
-  ssl: {rejectUnauthorized: false}
-}
-
-const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionProduction : connectionLocal)
-
+ const pool = new Pool({
+  connectionString
+ })
+ 
 module.exports = pool
-// pool is an object given to us by the pg library,
-// that allows us to run query command and returns a promise to give us that data.
+
+
+// const { Pool } = require('pg')
+
+// const connectionString = 'postgresql://postgres:f1ynjwcZwvCVSbPqlFAH@containers-us-west-32.railway.app:7290/railway'
+
+// // const connectionLocal = {
+//   //   user: 'postgres',
+//   //   host: 'localhost',
+//   //   database: 'assembly-backend',
+//   //   password: '',
+//   //   port: 5432,
+//   // }
+  
+//   const pool = new Pool({
+//     connectionString
+//   })
+  
+//   // const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionProduction : connectionLocal)
+
+// // this line of code is for heroku or netify to run the backend from a third party host
+// // const connectionProduction = {
+// //   connectionString: process.env.DATABASE_URL, 
+// //   ssl: {rejectUnauthorized: false}
+// // }
+
+
+// // module.exports = pool
+// // pool is an object given to us by the pg library,
+// // that allows us to run query command and returns a promise to give us that data.
+
+
+// // const { Pool } = require('pg')
+
+
+ 
+// module.exports = pool
