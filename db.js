ebc70ff602b30f37032d6ae15bc59845dfbe36b0
gavingiddings1997@gmail.com <Gavin>
2023-02-28 11:46:45 -0500

    connectionString

--- db.js
+++ db.js
@@ -1,46 +1,23 @@
 const { Pool } = require('pg')
 
-const connectionString = 'postgresql://postgres:f1ynjwcZwvCVSbPqlFAH@containers-us-west-32.railway.app:7290/railway'
+const connectionLocal = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'assembly-backend',
+  password: '',
+  port: 5432,
+}
 
- const pool = new Pool({
-  connectionString
- })
- 
+// this line of code is for heroku or netify to run the backend from a third party host
+const connectionProduction = {
+  connectionString: process.env.DATABASE_URL, 
+  ssl: {rejectUnauthorized: false}
+}
+
+const connectionString = "postgresql://postgres:zwZQJ7z1Z7BpZGJuWUNq@containers-us-west-125.railway.app:6560/railway"
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionString : connectionLocal)
+
 module.exports = pool
-
-
-// const { Pool } = require('pg')
-
-// const connectionString = 'postgresql://postgres:f1ynjwcZwvCVSbPqlFAH@containers-us-west-32.railway.app:7290/railway'
-
-// // const connectionLocal = {
-//   //   user: 'postgres',
-//   //   host: 'localhost',
-//   //   database: 'assembly-backend',
-//   //   password: '',
-//   //   port: 5432,
-//   // }
-  
-//   const pool = new Pool({
-//     connectionString
-//   })
-  
-//   // const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionProduction : connectionLocal)
-
-// // this line of code is for heroku or netify to run the backend from a third party host
-// // const connectionProduction = {
-// //   connectionString: process.env.DATABASE_URL, 
-// //   ssl: {rejectUnauthorized: false}
-// // }
-
-
-// // module.exports = pool
-// // pool is an object given to us by the pg library,
-// // that allows us to run query command and returns a promise to give us that data.
-
-
-// // const { Pool } = require('pg')
-
-
- 
-// module.exports = pool
+// pool is an object given to us by the pg library,
+// that allows us to run query command and returns a promise to give us that data.
